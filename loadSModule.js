04407aeb0260917229ea8546e225d537ba2237ee
Nicolas C.Barbosa <nicolascb.claudio@gmail.com>
2016-04-19 00:18:03 -0300

    Create loadSModule.js

---
+++ loadSModule.js
@@ -0,0 +1,21 @@
+function loadJS(src, callback){
+    script = doc.createElement('script');
+    script.async = true;
+    if (typeof(callback) == 'function'){
+        if (script.readyState){  //verifica de o browser possui suporte
+            script.onreadystatechange = function(){
+                if (script.readyState == 'loaded'|| script.readyState == 'complete'){
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        }
+        else{  //browsers antigos
+            script.onload = function(){
+                callback();
+            };
+        }
+    }
+    script.src = src;
+    scripts.parentNode.insertBefore(script, scripts);
+}
